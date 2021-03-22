39ad9004fa083c4624e7896c1c642057a93f72d3
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-23 04:31:50 +0500

    сжатие css ,js ,html

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,27 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+  return config
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -16,7 +37,10 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
@@ -26,6 +50,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist')
         }
       ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
     })
   ],
   resolve: {
@@ -35,19 +62,19 @@ module.exports = {
       '@': path.resolve(__dirname, 'src')
     }
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
+  optimization: optimization(),
   devServer: {
-    port: 4200
+    port: 4200,
+    hot: isDev
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'] //идет справа налево
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {},
+        }, 'css-loader'] //идет справа налево
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
