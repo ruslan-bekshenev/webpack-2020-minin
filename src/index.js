0f74d9ee79f95b8f4ba94ddbcd6929153d46d99a
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-23 03:35:41 +0500

    add alias

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import Post from './Post'
-import json from './assets/json.json'
-import WebpackLogo from './assets/webpack-logo.png'
-import xml from './assets/data.xml'
-import csv from './assets/data.csv'
+import Post from '@models/Post'
+import json from '@/assets/json'
+import WebpackLogo from '@/assets/webpack-logo'
+import xml from '@/assets/data.xml'
+import csv from '@/assets/data.csv'
 
 import './styles/style.css'
 const post = new Post('Webpack Post Title', WebpackLogo)
